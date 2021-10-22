8c0bad778318067614f3afdea375f922dd20e31b
Nam Luu <nam.luu@isobar.com>
2021-10-22 23:15:58 +0700

    admin menu minor style

--- resources/views/admin/menu/multiple.blade.php
+++ resources/views/admin/menu/multiple.blade.php
@@ -4,14 +4,21 @@
             <td>{{ $item->id }}</td>
             <td>{{ $char . $item->name }}</td>
             <td>{{ Str::of($item->description)->words(15, '...') }}</td>
-            <td>{{ $item->active }}</td>
+            <td>
+                @if ($item->active)
+                    <span class="btn btn-success btn-xs">YES</span>
+                @else
+                    <span class="btn btn-danger btn-xs">NO</span>
+                @endif
+            </td>
             <td>{{ $item->created_at }}</td>
             <td>
-                <a href="{{ route('menus.edit', $item->id)  }}"><i class="fas fa-edit"></i></a>&nbsp;|&nbsp;
-                <a href="#" onclick="removeRow({{$item->id}}, '{{ route('menus.delete')  }}')"><i class="fas fa-trash text-danger"></i></a>
+                <a href="{{ route('menus.edit', $item->id) }}" class="btn btn-primary btn-sm"><i class="fas fa-edit"></i></a>&nbsp;|&nbsp;
+                <a href="#" onclick="removeRow({{$item->id}}, '{{ route('menus.delete')  }}')" class="btn btn-danger btn-sm">
+                    <i class="fas fa-trash"></i></a>
             </td>
         </tr>
         @unset($menus[$loop->index])
-        @include('admin.menu.multiple', ['menus' => $menus, 'parent_id' => $item->id, 'char' => '--'])
+        @include('admin.menu.multiple', ['menus' => $menus, 'parent_id' => $item->id, 'char' => '|-- '])
     @endif
 @endforeach
