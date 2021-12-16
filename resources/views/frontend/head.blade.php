6b0c2e957a06f1aa9c3b2f2b1859e1b8aa967516
Nam Luu <nam.luuduc@gmail.com>
2021-12-16 23:49:37 +0700

    frontend - refactor theme

---
+++ resources/views/frontend/head.blade.php
@@ -0,0 +1,32 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->  
+<link rel="icon" type="image/png" href="/template/cozastore/images/icons/favicon.png"/>
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="/template/cozastore/vendor/bootstrap/css/bootstrap.min.css">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="/template/cozastore/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="/template/cozastore/fonts/iconic/css/material-design-iconic-font.min.css">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="/template/cozastore/fonts/linearicons-v1.0.0/icon-font.min.css">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="/template/cozastore/vendor/animate/animate.css">
+<!--===============================================================================================-->  
+<link rel="stylesheet" type="text/css" href="/template/cozastore/vendor/css-hamburgers/hamburgers.min.css">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="/template/cozastore/vendor/animsition/css/animsition.min.css">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="/template/cozastore/vendor/select2/select2.min.css">
+<!--===============================================================================================-->  
+<link rel="stylesheet" type="text/css" href="/template/cozastore/vendor/daterangepicker/daterangepicker.css">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="/template/cozastore/vendor/slick/slick.css">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="/template/cozastore/vendor/MagnificPopup/magnific-popup.css">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="/template/cozastore/vendor/perfect-scrollbar/perfect-scrollbar.css">
+<!--===============================================================================================-->
+<link rel="stylesheet" type="text/css" href="/template/cozastore/css/util.css">
+<link rel="stylesheet" type="text/css" href="/template/cozastore/css/main.css">
+<!--===============================================================================================-->
